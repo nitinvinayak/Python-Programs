5476f6d5ef30d7844d8cea08ca3b6b60cfd50b5b
Ieshaan Saxena <31623825+ieshaan12@users.noreply.github.com>
2018-06-04 21:49:29 +0530

    Update firstturtleprog.py

--- firstturtleprog.py
+++ firstturtleprog.py
@@ -1,17 +1,22 @@
 import turtle
 turtle.colormode(255);
 #turtle.color("blue");
-turtle.color(125,0,123);
-turtle.forward(50);
-turtle.left(72);
-turtle.forward(50);
-turtle.left(72);
-turtle.forward(50);
-turtle.left(72);
-turtle.forward(50);
-turtle.left(72);
-turtle.forward(50);
-turtle.left(72);
-turtle.left(30);
+for i in range(12):
+ turtle.color(123,20*i,0);
+ turtle.forward(50);
+ turtle.left(72);
+ turtle.penup();
+ turtle.forward(50);
+ turtle.left(72);
+ turtle.forward(50);
+ turtle.left(72);
+ turtle.pendown();
+ turtle.forward(50);
+ turtle.left(72);
+ turtle.penup();
+ turtle.forward(50);
+ turtle.left(72);
+ turtle.pendown();
+ turtle.left(30);
 turtle.shape("turtle");
 turtle.exitonclick();
