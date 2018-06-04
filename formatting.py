7d2ba9083da617fbb73341eb1c8673c8fefd9431
Ieshaan Saxena <31623825+ieshaan12@users.noreply.github.com>
2018-06-04 23:37:48 +0530

    Create formatting.py

---
+++ formatting.py
@@ -0,0 +1,6 @@
+x=24.432432432;
+print("Binary rep:{0:b}".format(12));
+print("Round off rep:{0:0.3f}".format(x));
+print("Exponential rep:{0:e}".format(x));
+#common python string methods
+#lower(),upper(),split(),join(),find(),replace().
