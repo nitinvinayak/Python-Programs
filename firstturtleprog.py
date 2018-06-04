e4d47254a6c87cae52273ea8ee613ef0b7afbc6b
Ieshaan Saxena <31623825+ieshaan12@users.noreply.github.com>
2018-06-04 20:51:27 +0530

    Create firstturtleprog.py
    This is a simple program that makes a pentagon using Turtle graphics.

---
+++ firstturtleprog.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.colormode(255);
+#turtle.color("blue");
+turtle.color(125,0,123);
+turtle.forward(50);
+turtle.left(72);
+turtle.forward(50);
+turtle.left(72);
+turtle.forward(50);
+turtle.left(72);
+turtle.forward(50);
+turtle.left(72);
+turtle.forward(50);
+turtle.left(72);
+turtle.left(30);
+turtle.shape("turtle");
+turtle.exitonclick();
