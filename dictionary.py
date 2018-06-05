dd3bcfed2b964ff8fd6e8e9deba2f0e0719f04bd
Ieshaan Saxena <31623825+ieshaan12@users.noreply.github.com>
2018-06-05 21:47:37 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,35 @@
+#Dictionaries
+d=dict(); #Create a New Dictionary
+#Generating key-value pairs
+d['name']='SpecialOne';
+d['id']=1232017;
+d['status']='pass';
+print(d);#Prints all key-value pairs
+print(d.keys());#Prints keys
+print(d.values());#Prints values
+print(d['name']);#Prints the value corresponding to key
+print(d.items());#Prints all the dictionary items of that name
+#This loop iterates through the dictionary
+for key,value in d.items():
+    print(key,' : ',value);
+name={'d':'SpecialOne'};
+name['c']='SimpleOne';
+print(name);
+del name;
+d.update({'id':2312312});#Updates value of id
+print(d.items());
+del d;
+#new Dictionary
+username={'Axel':'Axel10','Mark':'Evans1','Jude':'Jude14'};
+while True:
+    print("Enter a name:");
+    name=input();#Obtaining input from user.
+    if name in username:
+        print(username[name]+' : '+name);
+    else:
+        print("not in data, please input to add");
+        use=input()
+        username[name]=use;
+        print("updated");
+#No exit in above loop
+print(username);        
