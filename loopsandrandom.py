18c8edcd122fdda8afe311b1a93b71257ec814ea
Ieshaan Saxena <31623825+ieshaan12@users.noreply.github.com>
2018-06-09 00:11:57 +0530

    Update loopsandrandom.py

--- loopsandrandom.py
+++ loopsandrandom.py
@@ -7,11 +7,14 @@ for i in range(2,20,3):        #You can pass upto 3 arguments in range
     print(i);                  #Where as i is the iterating variable
                                #The args are(start,stop,step);Similar to C++ loops
                                #The stop value is not inclusive
+print(random.random());        #Prints a decimal value between 0 and 1
+print(random.choice(['q','r','w','s'])); #prints one of the items in the list
 nat_numbers=[];
 for i in range(1,10):
     nat_numbers.append(i);
 print(nat_numbers);
 letters=[];
+#Type Casting Done
 for i in range(1,27):
     print(str(i)+"th letter is "+chr(i+96));#Data type converstion- int to char
     letters.append(chr(i+96));
